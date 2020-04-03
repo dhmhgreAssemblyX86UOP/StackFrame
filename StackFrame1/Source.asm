cc9acf2d16d57792ae60f56ca3fa10d7aa85364e
Grigoris Dimitroulakos <dhmhgre@gmail.com>
2020-04-03 12:29:04 +0300

    Stack Frame Creation Step 1: Push the argument to the stack Comments added

--- StackFrame1/Source.asm
+++ StackFrame1/Source.asm
@@ -12,8 +12,10 @@ AddTwo PROTO
 
 .code
 ; **************************************************************
-; Procedure name : AddTwo
+; Procedure name : AddTwo   
 ; Description : Adds two numbers and returns the sum
+;				 procedure equivalent in C language 
+;			   	 void AddTwo(int a,int b, int*res)
 ; Input		  : The first 2 arguments of the procedure are
 ;				the two addents 
 ; Output	  : The third argument is returned by reference to 
@@ -28,13 +30,20 @@ AddTwo ENDP
 ; **************************************************************
 ; Procedure name : main ( Startup Function )
 ; Description : Calls the AddTwo procedure giving two numbers 
+;				procedure equivalent in C language 
+;			   	void main()
 ;				and prints the result to the screen. 
 ; Input		  : - 
 ; Output	  : -
 ; **************************************************************
 main PROC
-
-
+	; Step 1 : Pass arguments to the stack
+	; Arguments are pushed in the opposite sequence than 
+	; the C language equivalent call of AddTwo(5,6,&res)
+	push OFFSET res		; pass the address of res variable ( pass by reference)
+	push 6
+	push 5
 exit
+
 main ENDP
 END main
