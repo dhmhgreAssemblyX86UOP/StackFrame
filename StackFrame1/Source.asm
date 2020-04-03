19a8a467c971724ad65ca8d769b80bdd39f9e773
Grigoris Dimitroulakos <dhmhgre@gmail.com>
2020-04-03 12:22:09 +0300

    Declare the variable to store the result Comments added

--- StackFrame1/Source.asm
+++ StackFrame1/Source.asm
@@ -3,14 +3,35 @@ INCLUDE Irvine32.inc
 
 .data
 
+; Declare a variable to return the value from the
+; AddTwo procedure
+res DWORD ?
+
+; AddTwo procedure declaration
 AddTwo PROTO
 
 .code
+; **************************************************************
+; Procedure name : AddTwo
+; Description : Adds two numbers and returns the sum
+; Input		  : The first 2 arguments of the procedure are
+;				the two addents 
+; Output	  : The third argument is returned by reference to 
+;				the caller
+; **************************************************************
 AddTwo PROC
 
 
 	ret
 AddTwo ENDP
+
+; **************************************************************
+; Procedure name : main ( Startup Function )
+; Description : Calls the AddTwo procedure giving two numbers 
+;				and prints the result to the screen. 
+; Input		  : - 
+; Output	  : -
+; **************************************************************
 main PROC
 
 
